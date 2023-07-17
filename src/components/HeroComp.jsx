99c4646cb82593ee855a6234e2bfcd6f6b6bdb48
Fauza Rizky <fauzasmg@gmail.com>
2023-07-17 08:21:03 +0700

    make HeroComp

--- src/components/HeroComp.jsx
+++ src/components/HeroComp.jsx
@@ -1,7 +1,18 @@
 import { Container, Row, Col } from "react-bootstrap";
 
 const HeroComp = () => {
-  return <div></div>;
+  return (
+    <div className="hero min-vh-100 w-100">
+      <Container>
+        <Row>
+          <Col className="text-white text-center">
+            <h1>Mari Bersantai Sebentar</h1>
+            <p className="text-white-50">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, sequi.</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default HeroComp;
