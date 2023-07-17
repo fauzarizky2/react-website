7a06f37b760bdecae2ab98bd769ff3568760f9f9
Fauza Rizky <fauzasmg@gmail.com>
2023-07-17 09:29:57 +0700

    Make GalleryComp & modified h1 and p class at HeroComp

--- src/components/HeroComp.jsx
+++ src/components/HeroComp.jsx
@@ -5,9 +5,9 @@ const HeroComp = () => {
     <div className="hero min-vh-100 w-100">
       <Container>
         <Row>
-          <Col className="text-white text-center">
-            <h1>Mari Bersantai Sebentar</h1>
-            <p className="text-white-50">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, sequi.</p>
+          <Col>
+            <h1 className="text-white text-center fs-1">Mari Bersantai Sebentar</h1>
+            <p className="text-white text-center text-white-50">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, sequi.</p>
           </Col>
         </Row>
       </Container>
